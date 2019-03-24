8e5aa648f18ddea87583df296d8d3fa3cf920b9e
Armor9D <46793521+Armor9D@users.noreply.github.com>
2019-03-23 19:12:43 -0700

    0.1
    Scaling movement demonstration for parallax.  Need to figure out how to layer objects.

---
+++ Homework 4/CirclePanel.java
@@ -0,0 +1,68 @@
+import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+class CirclePanel extends JPanel
+{
+	private int defaultX = 120; //SquarePanel's X position when initialized
+	private int defaultY = 60; //SquarePanel's Y position when initialized
+	private int X = defaultX;
+	private int Y = defaultY;
+	private int width = 60; // width of rectangle
+	private int height = 60; // height of rectangle
+	
+	public CirclePanel()
+	{
+		addMouseListener(new MyMouseListener());
+		addMouseMotionListener(new MyMouseMotionListener());
+	}
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		
+		g.setColor (Color.black);
+		g.drawOval(X, Y, width, height);
+	}
+	
+	private class MyMouseListener implements MouseListener
+	{
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		X = defaultX;
+		Y = defaultY;
+		
+		repaint();
+	}
+	}
+	
+	private class MyMouseMotionListener implements MouseMotionListener
+	{
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		X = (e.getX() + 150) / 2 - 30;
+		
+		repaint();
+		//can be scaled
+	}
+	}
+}
---
+++ Homework 4/ParallaxDemo.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ParallaxDemo
+{
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame("Parallax Demo");
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		SquarePanel square1 = new SquarePanel();
+		CirclePanel circle1 = new CirclePanel();
+		frame.add(square1);
+		frame.add(circle1, 1);
+		
+		frame.setSize(300, 300); // set frame size
+		frame.setVisible(true); // display frame
+	}
+}
